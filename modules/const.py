954bf614c6ad6b335d709e6a9fb6f92debad9fd6
kukushkinace1 <122355565+kukushkinace1@users.noreply.github.com>
2023-12-29 13:19:28 +0300

    Update const.py

--- modules/const.py
+++ modules/const.py
@@ -42,7 +42,7 @@ DATA = {
     'ethereum':     {'rpc': ['https://rpc.ankr.com/eth'],       'scan': 'https://etherscan.io/tx',              'token': 'ETH', 'chain_id': 1},
     'optimism':     {'rpc': ['https://rpc.ankr.com/optimism'],  'scan': 'https://optimistic.etherscan.io/tx',   'token': 'ETH', 'chain_id': 10},
     'bsc':          {'rpc': ['https://rpc.ankr.com/bsc'],       'scan': 'https://bscscan.com/tx',               'token': 'BNB', 'chain_id': 56},
-    'polygon':      {'rpc': ['https://rpc.ankr.com/polygon'],   'scan': 'https://polygonscan.com/tx',           'token': 'MATIC','chain_id': 137},
+    'polygon':      {'rpc': ['https://rpc.ankr.com/polygon', 'https://endpoints.omniatech.io/v1/matic/mainnet/public'],   'scan': 'https://polygonscan.com/tx',           'token': 'MATIC','chain_id': 137},
     'polygon zkevm':{'rpc': ['https://zkevm-rpc.com'],          'scan': 'https://zkevm.polygonscan.com/tx',     'token': 'ETH', 'chain_id': 1101},
     'arbitrum':     {'rpc': ['https://rpc.ankr.com/arbitrum'],  'scan': 'https://arbiscan.io/tx',               'token': 'ETH', 'chain_id': 42161},
     'avalanche':    {'rpc': ['https://rpc.ankr.com/avalanche'], 'scan': 'https://snowtrace.io/tx',              'token': 'AVAX','chain_id': 43114},
